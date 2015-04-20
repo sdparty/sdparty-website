f87bbbe516f9c4e144c3f71ce3713df0b9ab81e0
billy3321 <billy3321@gmail.com>
2015-04-21 01:32:41 +0800

    add new content

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -1,2 +1,57 @@
 class BannersController < ApplicationController
+  before_action :set_banner, except: [:index, :new]
+
+  # GET /banners
+  def index
+    @banners = Banner.all.page params[:page]
+  end
+
+  # GET /banners/1
+  def show
+  end
+
+  # GET /banners/new
+  def new
+    @banner = Banner.new
+  end
+
+  # GET /banners/1/edit
+  def edit
+  end
+
+  # POST /banners
+  def create
+    if @banner.save
+        redirect_to @banner, notice: '關鍵字建立成功'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /banners/1
+  def update
+    if @banner.update(banner_params)
+      redirect_to @banner, notice: '關鍵字更新成功'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /banners/1
+  def destroy
+    @banner.destroy
+    redirect_to banners_url, notice: '關鍵字已刪除'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_banner
+    @banner = params[:id] ? Banner.find(params[:id]) : Banner.new(banner_params)
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def banner_params
+    params.require(:banner).permit(:position, :image, :title, :link, :published)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,49 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable,
          :validatable, :lockable, :omniauthable
   has_many :articles
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :provider_uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        registered_user.provider = auth.provider
+        registered_user.provider_uid = auth.uid
+        registered_user.save
+        return registered_user
+      else
+        user = User.create(name:auth.info.name,
+                            provider:auth.provider,
+                            provider_uid:auth.uid,
+                            email:auth.info.email,
+                            password:Devise.friendly_token[0,20]
+                          )
+      end
+    end
+  end
+
+  def self.find_for_google_oauth2(auth, signed_in_resource=nil)
+    data = auth.info
+    user = User.where(:provider => auth.provider, :provider_uid => auth.uid ).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        registered_user.provider = auth.provider
+        registered_user.provider_uid = auth.uid
+        registered_user.save
+        return registered_user
+      else
+        user = User.create(name: data["name"],
+          provider:auth.provider,
+          email: data["email"],
+          provider_uid: auth.uid ,
+          password: Devise.friendly_token[0,20]
+        )
+      end
+    end
+  end
 end
