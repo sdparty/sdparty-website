a0c83c614e02c2c57034d8a687f529746eacdca8
billy3321 <billy3321@gmail.com>
2015-05-03 13:10:13 +0800

    fix ckeditor error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( admin.js )
 Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+Rails.application.config.assets.precompile += Ckeditor.assets
+Rails.application.config.assets.precompile += %w( ckeditor/* )
