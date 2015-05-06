a9c0ca87b5de3831daf39813f0a02d50602f5606
billy3321 <billy3321@gmail.com>
2015-05-06 22:41:07 +0800

    fix banner factory

--- spec/factories/banners.rb
+++ spec/factories/banners.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     sequence(:title) { |n| "Title #{n}" }
     sequence(:button) { |n| "Button #{n}" }
     link "http://www.google.com/"
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'test.jpg')) }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'banner.jpg')) }
     published true
   end
 end
