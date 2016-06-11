33ce5b398f210ab99d1126f1412d0752830fa346
billy3321 <billy3321@gmail.com>
2016-06-11 17:18:48 +0800

    remove candidate spec

--- spec/requests/candidates_controller_spec.rb
+++ spec/requests/candidates_controller_spec.rb
@@ -2,42 +2,42 @@ require "rails_helper"
 
 describe "Candidate" do
 
-  let(:candidate) { FactoryGirl.create(:candidate) }
+  # let(:candidate) { FactoryGirl.create(:candidate) }
 
-  describe "#index with nothing" do
-    it "success" do
-      get "/candidates/"
-      expect(response).to be_success
-    end
-  end
+  # describe "#index with nothing" do
+  #   it "success" do
+  #     get "/candidates/"
+  #     expect(response).to be_success
+  #   end
+  # end
 
-  describe "#index" do
-    it "success" do
-      2.times { FactoryGirl.create(:candidate) }
-      get "/candidates/"
-      expect(response).to be_success
-    end
-  end
+  # describe "#index" do
+  #   it "success" do
+  #     2.times { FactoryGirl.create(:candidate) }
+  #     get "/candidates/"
+  #     expect(response).to be_success
+  #   end
+  # end
 
-  describe "#proportional" do
-    it "success" do
-      2.times { FactoryGirl.create(:candidate) }
-      get "/candidates/proportional"
-      expect(response).to be_success
-    end
-  end
+  # describe "#proportional" do
+  #   it "success" do
+  #     2.times { FactoryGirl.create(:candidate) }
+  #     get "/candidates/proportional"
+  #     expect(response).to be_success
+  #   end
+  # end
 
-  describe "#show" do
-    it "success" do
-      get "/candidates/#{candidate.id}"
-      expect(response).to be_success
-    end
-  end
+  # describe "#show" do
+  #   it "success" do
+  #     get "/candidates/#{candidate.id}"
+  #     expect(response).to be_success
+  #   end
+  # end
 
-  describe "#donate" do
-    it "success" do
-      get "/candidates/#{candidate.id}/donate"
-      expect(response).to be_success
-    end
-  end
+  # describe "#donate" do
+  #   it "success" do
+  #     get "/candidates/#{candidate.id}/donate"
+  #     expect(response).to be_success
+  #   end
+  # end
 end
