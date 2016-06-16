4e8d2fb94bdf615602af3c18cbe6afb6646497d0
billy3321 <billy3321@gmail.com>
2016-06-16 09:11:44 +0800

    add modernizr

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require modernizr
 //= require jquery.themepunch.tools.min
 //= require jquery.themepunch.revolution
 //= require isotope.pkgd
