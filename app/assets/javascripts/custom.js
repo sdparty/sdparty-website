ba73082f5e10ba27d140995d3b8ade1dbf5dd4d9
YuTin Liu <yuting1987@gmail.com>
2016-09-19 08:41:11 +0800

    fix(static page): fix donate_one custom

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -25,22 +25,25 @@ $('#donate-one').submit(function(e) {
     query['project_id'] = '274';
     query['reward_id'] = '1153';
     input = ['custom_field[1813]', 'custom_field[1814]', 'custom_field[1815]', 'custom_field[1816]', 'custom_field[1817]'];
+    query['custom_field[1818]'] = '指定社民黨使用'
   } else {
     query['project_id'] = '74';
     query['reward_id'] = '247';
     input = ['custom_field[130]', 'custom_field[131]', 'custom_field[133]', 'custom_field[134]', 'custom_field[135]'];
+    query['custom_field[136]'] = '指定社民黨使用'
   }
 
-  $('table input', form).each(function(idx, item) {
+  $('.donate-input-group input', form).each(function(idx, item) {
     if (input[idx]) {
       query[input[idx]] = $(item).val();
     }
   });
 
+
   query['email'] = $('[name="email"]', form).val();
   query['additional_support'] = $('[name="additional_support"]', form).val();
 
-  location.href = 'http://sdparty.backme.tw:80/cashflow/checkout?' + $.param(query);
+  location.href = 'http://sdparty.backme.tw/cashflow/checkout?' + $.param(query);
 
   return false;
 });
