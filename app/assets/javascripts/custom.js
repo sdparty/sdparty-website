e5746548508f992457806751a7d4fa89643696e1
YuTin Liu <yuting1987@gmail.com>
2017-11-01 23:55:46 +0800

    feat(web): update donate

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -44,16 +44,32 @@ $('#donate-one').submit(function(e) {
   var form = $('#donate-one');
   var query = {};
 
+  var memo = '指定社民黨使用；';
+
+  if (parseInt($('[name="anonymous"]:checked', form).val(), 10)) {
+    memo += '匿名捐款；';
+  } else {
+    memo += '本名捐款；';
+  }
+
+  if (parseInt($('[name="receipt"]:checked', form).val(), 10)) {
+    memo += '年底寄送；';
+  } else {
+    memo += '不需寄送；';
+  }
+
+  memo += '介紹人：' + $('[name="referrals"]', form).val();
+
   if ($('[name="tax"]:checked', form).val() === '1') {
     query['project_id'] = '274';
     query['reward_id'] = '1153';
     input = ['custom_field[1813]', 'custom_field[1814]', 'custom_field[1815]', 'custom_field[1816]', 'custom_field[1817]'];
-    query['custom_field[1818]'] = '指定社民黨使用'
+    query['custom_field[1818]'] = memo;
   } else {
     query['project_id'] = '74';
     query['reward_id'] = '247';
     input = ['custom_field[130]', 'custom_field[131]', 'custom_field[133]', 'custom_field[134]', 'custom_field[135]'];
-    query['custom_field[136]'] = '指定社民黨使用'
+    query['custom_field[136]'] = memo;
   }
 
   $('.donate-input-group input', form).each(function(idx, item) {
@@ -61,7 +77,6 @@ $('#donate-one').submit(function(e) {
       query[input[idx]] = $(item).val();
     }
   });
-
 
   query['email'] = $('[name="email"]', form).val();
   query['additional_support'] = $('[name="additional_support"]', form).val();
